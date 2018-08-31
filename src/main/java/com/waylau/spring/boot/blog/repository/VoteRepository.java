082f237efef00a3758e01fc17af654518e3b9039
muzixiaozhe <lipeizhe@126.com>
2018-09-01 00:25:34 +0800

    blog site

---
+++ src/main/java/com/waylau/spring/boot/blog/repository/VoteRepository.java
@@ -0,0 +1,12 @@
+package com.waylau.spring.boot.blog.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.waylau.spring.boot.blog.domain.Vote;
+
+/**
+ * Vote 仓库.
+ */
+public interface VoteRepository extends JpaRepository<Vote, Long>{
+ 
+}
---
+++ src/main/java/com/waylau/spring/boot/blog/service/AuthorityService.java
@@ -0,0 +1,17 @@
+package com.waylau.spring.boot.blog.service;
+
+import com.waylau.spring.boot.blog.domain.Authority;
+
+/**
+ * Authority 服务接口.
+ */
+public interface AuthorityService {
+	 
+	
+	/**
+	 * 根据id获取 Authority
+	 * @param Authority
+	 * @return
+	 */
+	Authority getAuthorityById(Long id);
+}
